ef022f078027ab9e5d5373a8d1556b17e9b127bc
david <david>
1998-04-15 20:01:09 +0000

    *** empty log message ***

--- Documentation/test_smorgasbord.pl
+++ Documentation/test_smorgasbord.pl
@@ -10,8 +10,8 @@
 #
 #--------------------------------------------------------
 
-    use LayoutInclude;
-    use Utils;
+    use ILT::LayoutInclude;
+    use ILT::LayoutUtils;
 
 #-------------------------------------------------------
 #
@@ -24,23 +24,21 @@
 
     $n_rows = 3;
     $n_cols = 4;
-    $layout = ImageLayout->new( $n_rows, $n_cols );
+    $layout = ILT::ImageLayout->new_grid( $n_rows, $n_cols );
 
-    $layout->output_filename( "output.rgb" );
-
-    $layout->output_size( 600, 600 );
-    $layout->horizontal_white_space( 10 );
-    $layout->vertical_white_space( 8 );
+    $layout->horizontal_white_space( 3 );
+    $layout->vertical_white_space( 3 );
     $layout->white_space_colour( "dim_gray" );
 
     for( $row = 0;  $row < $n_rows;  ++$row )
     {
         for( $col = 0;  $col < $n_cols;  ++$col )
         {
-            $view = View->new( "top" );
-            $surface_object = GeometricObject->new( "Data/surf1.obj" );
-            $image_info = ImageInfo->new( $surface_object, $view );
-            $layout->image_info( $row, $col, $image_info );
+            $view = ILT::View->new_canonical( Top_view );
+            $surface_object = ILT::GeometricObject->new( "Data/surf1.obj" );
+            $image_info = ILT::ImageInfo->new( $surface_object, $view );
+            $layout->image_info( $layout->row_col_to_index($row,$col),
+                                 $image_info );
         }
     }
 
@@ -50,12 +48,13 @@
 #
 #--------------------------------------------------------------------------
 
-    $plane_object = PlaneObject->new( "x", -10 );
-    $view = View->new( "left" );
-    $volume_object = VolumeObject->new( "Data/volume1.mnc" );
-    $scene_object = IntersectionObject->new( $plane_object, $volume_object );
-    $image_info = ImageInfo->new( $scene_object, $view );
-    $layout->image_info( 0, 0, $image_info );
+    $plane_object = ILT::PlaneObject->new_canonical( Sagittal_axis, -10 );
+    $view = ILT::View->new_canonical( Left_view );
+    $volume_object = ILT::VolumeObject->new( "Data/volume1.mnc" );
+    $scene_object = ILT::IntersectionObject->new( $plane_object,
+                                                  $volume_object );
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(0,0), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -63,18 +62,18 @@
 #
 #--------------------------------------------------------------------------
 
-    $plane_object = PlaneObject->new( "x", -10 );
-    $view = View->new( "left" );
-    $volume_object = VolumeObject->new( "Data/volume1.mnc" );
+    $plane_object = ILT::PlaneObject->new_canonical( Sagittal_axis, -10 );
+    $view = ILT::View->new_canonical( Left_view );
+    $volume_object = ILT::VolumeObject->new( "Data/volume1.mnc" );
 
     $scene_object = 
-                  ColourObject->new(
-                       IntersectionObject->new( $plane_object,
-                                                $volume_object ),
+                  ILT::ColourObject->new_volume_colouring(
+                       ILT::IntersectionObject->new( $plane_object,
+                                                     $volume_object ),
                        $volume_object, Hot_metal_scale, 50, 150 );
 
-    $image_info = ImageInfo->new( $scene_object, $view );
-    $layout->image_info( 0, 1, $image_info );
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(0,1), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -82,34 +81,34 @@
 #
 #--------------------------------------------------------------------------
 
-    $plane_object1 = PlaneObject->new( "x", 0 );
-    $plane_object2 = PlaneObject->new( "y", 0 );
-    $plane_object3 = PlaneObject->new( "z", 0 );
-    $view = View->new( -1, -2, -1, 0, 0, 1 );
-    $volume_object = VolumeObject->new( "Data/volume1.mnc" );
+    $plane_object1 = ILT::PlaneObject->new_canonical( Sagittal_axis, 0 );
+    $plane_object2 = ILT::PlaneObject->new_canonical( Coronal_axis, 0 );
+    $plane_object3 = ILT::PlaneObject->new_canonical( Transverse_axis, 0 );
+    $view = ILT::View->new_arbitrary( -1, -2, -1, 0, 0, 1 );
+    $volume_object = ILT::VolumeObject->new( "Data/volume1.mnc" );
 
-    $coloured_slice1 = ColourObject->new(
-                           IntersectionObject->new( $plane_object1,
-                                                    $volume_object ),
+    $coloured_slice1 = ILT::ColourObject->new_volume_colouring(
+                           ILT::IntersectionObject->new( $plane_object1,
+                                                         $volume_object ),
                            $volume_object, Hot_metal_scale, 50, 150 );
-    $coloured_slice2 = ColourObject->new(
-                           IntersectionObject->new( $plane_object2,
-                                                    $volume_object ),
+    $coloured_slice2 = ILT::ColourObject->new_volume_colouring(
+                           ILT::IntersectionObject->new( $plane_object2,
+                                                         $volume_object ),
                            $volume_object, Hot_metal_scale, 50, 150 );
-    $coloured_slice3 = ColourObject->new(
-                           IntersectionObject->new( $plane_object3,
-                                                    $volume_object ),
+    $coloured_slice3 = ILT::ColourObject->new_volume_colouring(
+                           ILT::IntersectionObject->new( $plane_object3,
+                                                         $volume_object ),
                            $volume_object, Hot_metal_scale, 50, 150 );
 
-    $union = UnionObject->new( $coloured_slice1,
+    $union = ILT::UnionObject->new( $coloured_slice1,
                                $coloured_slice2,
                                $coloured_slice3 );
 
-    $scene_object = RenderObject->new( $union );
+    $scene_object = ILT::RenderObject->new( $union );
     $scene_object->lighting_state( True );
 
-    $image_info = ImageInfo->new( $scene_object, $view );
-    $layout->image_info( 0, 2, $image_info );
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(0,2), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -117,25 +116,26 @@
 #
 #--------------------------------------------------------------------------
 
-    $plane_object = PlaneObject->new( "x", -10 );
-    $view = View->new( "left" );
-    $mri_volume = VolumeObject->new( "Data/volume1.mnc" );
-    $pet_volume = VolumeObject->new(
+    $plane_object = ILT::PlaneObject->new_canonical( Sagittal_axis, -10 );
+    $view = ILT::View->new_canonical( Left_view );
+    $mri_volume = ILT::VolumeObject->new( "Data/volume1.mnc" );
+    $pet_volume = ILT::VolumeObject->new(
            "/avgbrain/brain/images/mni_demo_fdg_normal_pet_tal.mnc.gz" );
-    $slice_object = IntersectionObject->new( $plane_object,
+    $slice_object = ILT::IntersectionObject->new( $plane_object,
                                              $mri_volume ),
 
 
-    $coloured_mri = ColourObject->new( $slice_object,
+    $coloured_mri = ILT::ColourObject->new_volume_colouring( $slice_object,
                            $mri_volume, Gray_scale, 50, 150 );
 
-    $scene_object = ColourObject->new( $coloured_mri,
-                                       $pet_volume, Spectral_scale, 400, 500 );
+    $scene_object = ILT::ColourObject->new_volume_colouring( $coloured_mri,
+                                      $pet_volume, Spectral_scale,
+                                      400, 500 );
     $scene_object->under_colour( "transparent" );
     $scene_object->opacity( .5 );
 
-    $image_info = ImageInfo->new( $scene_object, $view );
-    $layout->image_info( 0, 3, $image_info );
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(0,3), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -143,14 +143,14 @@
 #
 #--------------------------------------------------------------------------
 
-    $surface_object = GeometricObject->new( "Data/surf2.obj" );
-    $view = View->new( "right" );
+    $surface_object = ILT::GeometricObject->new( "Data/surf2.obj" );
+    $view = ILT::View->new_canonical( Right_view );
 
-    $scene_object = RenderObject->new( $surface_object );
+    $scene_object = ILT::RenderObject->new( $surface_object );
     $scene_object->lighting_state( True );
 
-    $image_info = ImageInfo->new( $scene_object, $view );
-    $layout->image_info( 1, 0, $image_info );
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(1,0), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -158,15 +158,15 @@
 #
 #--------------------------------------------------------------------------
 
-    $plane_object = PlaneObject->new( "x", -40 );
-    $surface_object = GeometricObject->new( "Data/surf2.obj" );
-    $view = View->new( "right" );
+    $plane_object = ILT::PlaneObject->new_canonical( Sagittal_axis, -40 );
+    $surface_object = ILT::GeometricObject->new( "Data/surf2.obj" );
+    $view = ILT::View->new_canonical( Right_view );
 
-    $cross = IntersectionObject->new( $plane_object, $surface_object ),
+    $cross = ILT::IntersectionObject->new( $plane_object, $surface_object ),
     $scene_object = $cross;
 
-    $image_info = ImageInfo->new( $scene_object, $view );
-    $layout->image_info( 1, 1, $image_info );
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(1,1), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -175,22 +175,22 @@
 #
 #--------------------------------------------------------------------------
 
-    $plane_object = PlaneObject->new( "x", -40 );
-    $surface_object = GeometricObject->new( "Data/surf2.obj" );
-    $volume_object = VolumeObject->new( "Data/volume1.mnc" );
-    $view = View->new( "right" );
+    $plane_object = ILT::PlaneObject->new_canonical( Sagittal_axis, -40 );
+    $surface_object = ILT::GeometricObject->new( "Data/surf2.obj" );
+    $volume_object = ILT::VolumeObject->new( "Data/volume1.mnc" );
+    $view = ILT::View->new_canonical( Right_view );
 
-    $cross = IntersectionObject->new( $plane_object, $surface_object ),
+    $cross = ILT::IntersectionObject->new( $plane_object, $surface_object );
 
-    $coloured_slice = ColourObject->new(
-                           IntersectionObject->new( $plane_object,
+    $coloured_slice = ILT::ColourObject->new_volume_colouring(
+                           ILT::IntersectionObject->new( $plane_object,
                                                     $volume_object ),
                            $volume_object, Gray_scale, 50, 150 );
 
-    $scene_object = UnionObject->new( $cross, $coloured_slice );
+    $scene_object = ILT::UnionObject->new( $cross, $coloured_slice );
 
-    $image_info = ImageInfo->new( $scene_object, $view );
-    $layout->image_info( 1, 2, $image_info );
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(1,2), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -198,20 +198,21 @@
 #
 #--------------------------------------------------------------------------
 
-    $surface_object = GeometricObject->new( "Data/surf2.obj" );
-    $volume_object = VolumeObject->new(
+    $surface_object = ILT::GeometricObject->new( "Data/surf2.obj" );
+    $volume_object = ILT::VolumeObject->new(
            "/avgbrain/brain/images/mni_demo_fdg_normal_pet_tal.mnc.gz" );
-    $view = View->new( "left" );
+    $view = ILT::View->new_canonical( Left_view );
 
-    $coloured_surface = ColourObject->new( $surface_object,
+    $coloured_surface = ILT::ColourObject->new_volume_colouring(
+                             $surface_object,
                              $volume_object, Spectral_scale, 300, 350 );
     $coloured_surface->under_colour( "white" );
 
-    $scene_object = RenderObject->new( $coloured_surface );
+    $scene_object = ILT::RenderObject->new( $coloured_surface );
     $scene_object->lighting_state( True );
 
-    $image_info = ImageInfo->new( $scene_object, $view );
-    $layout->image_info( 1, 3, $image_info );
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(1,3), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -219,27 +220,28 @@
 #
 #--------------------------------------------------------------------------
 
-    $plane_object1 = PlaneObject->new( "x", -50 );
-    $plane_object2 = PlaneObject->new( "x", -25 );
-    $plane_object3 = PlaneObject->new( "x", 0 );
-    $plane_object4 = PlaneObject->new( "x", 25 );
-    $plane_object5 = PlaneObject->new( "x", 50 );
-    $surface_object = GeometricObject->new( "Data/surf2.obj" );
-    $view = View->new( 1, .5, 0, 0, 0, 1 );
+    $plane_object1 = ILT::PlaneObject->new_canonical( Sagittal_axis, -50 );
+    $plane_object2 = ILT::PlaneObject->new_canonical( Sagittal_axis, -25 );
+    $plane_object3 = ILT::PlaneObject->new_canonical( Sagittal_axis, 0 );
+    $plane_object4 = ILT::PlaneObject->new_canonical( Sagittal_axis, 25 );
+    $plane_object5 = ILT::PlaneObject->new_canonical( Sagittal_axis, 50 );
+    $surface_object = ILT::GeometricObject->new( "Data/surf2.obj" );
+    $view = ILT::View->new_arbitrary( 1, .5, 0, 0, 0, 1 );
 
-    $cross1 = IntersectionObject->new( $plane_object1, $surface_object ),
-    $cross2 = IntersectionObject->new( $plane_object2, $surface_object ),
-    $cross3 = IntersectionObject->new( $plane_object3, $surface_object ),
-    $cross4 = IntersectionObject->new( $plane_object4, $surface_object ),
-    $cross5 = IntersectionObject->new( $plane_object5, $surface_object ),
+    $cross1 = ILT::IntersectionObject->new( $plane_object1, $surface_object ),
+    $cross2 = ILT::IntersectionObject->new( $plane_object2, $surface_object ),
+    $cross3 = ILT::IntersectionObject->new( $plane_object3, $surface_object ),
+    $cross4 = ILT::IntersectionObject->new( $plane_object4, $surface_object ),
+    $cross5 = ILT::IntersectionObject->new( $plane_object5, $surface_object ),
 
-    $union = UnionObject->new( $cross1, $cross2, $cross3, $cross4, $cross5 );
-    $render_object = RenderObject->new( $union );
+    $union = ILT::UnionObject->new( $cross1, $cross2, $cross3, $cross4,
+                                    $cross5 );
+    $render_object = ILT::RenderObject->new( $union );
     $render_object->lighting_state( True );
     $render_object->line_width( 2 );
 
-    $image_info = ImageInfo->new( $render_object, $view );
-    $layout->image_info( 2, 0, $image_info );
+    $image_info = ILT::ImageInfo->new( $render_object, $view );
+    $layout->image_info( $layout->row_col_to_index(2,0), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -247,4 +249,4 @@
 #
 #--------------------------------------------------------------------------
 
-    $layout->generate_image();
+    $layout->generate_image( "smorgasbord.rgb", 900, 600 );
