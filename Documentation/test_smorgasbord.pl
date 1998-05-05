daea5604e096e497aee28510209b7fcafcbaa2cf
david <david>
1998-05-05 18:45:56 +0000

    *** empty log message ***

--- Documentation/test_smorgasbord.pl
+++ Documentation/test_smorgasbord.pl
@@ -216,7 +216,7 @@
 
 #--------------------------------------------------------------------------
 #
-#   image[2][0] is a set of cross sections of a surface
+#   image[2][0] is a set of fat cross sections of a surface
 #
 #--------------------------------------------------------------------------
 
@@ -238,10 +238,113 @@
                                     $cross5 );
     $render_object = ILT::RenderObject->new( $union );
     $render_object->lighting_state( True );
-    $render_object->line_width( 2 );
+    $render_object->line_width( 5 );
 
     $image_info = ILT::ImageInfo->new( $render_object, $view );
     $layout->image_info( $layout->row_col_to_index(2,0), $image_info );
+
+#--------------------------------------------------------------------------
+#
+#   image[2][1] is several cross sections of a surface superimposed on a
+#   colour_coded slice, where a user defined colour map is used to create
+#   a contour-type image
+#
+#--------------------------------------------------------------------------
+
+    $plane_object1 = ILT::PlaneObject->new_canonical( Transverse_axis, 10 );
+    $plane_object2 = ILT::PlaneObject->new_canonical( Transverse_axis, 30 );
+    $plane_object3 = ILT::PlaneObject->new_canonical( Transverse_axis, 50 );
+    $surface_object = ILT::GeometricObject->new( "Data/surf2.obj" );
+    $volume_object = ILT::VolumeObject->new( "Data/volume1.mnc" );
+    $view = ILT::View->new_canonical( Top_view );
+
+    $cross1 = ILT::IntersectionObject->new( $plane_object1,
+                                            $surface_object );
+    $cross2 = ILT::IntersectionObject->new( $plane_object2,
+                                            $surface_object );
+    $cross3 = ILT::IntersectionObject->new( $plane_object3,
+                                            $surface_object );
+
+    $coloured_slice = ILT::ColourObject->new_volume_colouring(
+                           ILT::IntersectionObject->new( $plane_object1,
+                                                    $volume_object ),
+                           $volume_object, Usercc_scale, 0, 180 );
+    $coloured_slice->usercc_filename( "Data/contour.map" );
+
+    $scene_object = ILT::UnionObject->new( $cross1, $cross2, $cross3,
+                                           $coloured_slice );
+
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(2,1), $image_info );
+
+#--------------------------------------------------------------------------
+#
+#   image[2][2] is three colour-coded slices with a transparent under colour
+#   and a surface in a three d view
+#
+#--------------------------------------------------------------------------
+
+    $plane_object1 = ILT::PlaneObject->new_canonical( Sagittal_axis, 0 );
+    $plane_object2 = ILT::PlaneObject->new_canonical( Coronal_axis, 0 );
+    $plane_object3 = ILT::PlaneObject->new_canonical( Transverse_axis, 0 );
+    $view = ILT::View->new_arbitrary( -1, -2, -1, 0, 0, 1 );
+    $volume_object = ILT::VolumeObject->new( "Data/volume1.mnc" );
+
+    $coloured_slice1 = ILT::ColourObject->new_volume_colouring(
+                           ILT::IntersectionObject->new( $plane_object1,
+                                                         $volume_object ),
+                           $volume_object, Hot_metal_scale, 50, 150 );
+    $coloured_slice1->under_colour( "transparent" );
+
+    $coloured_slice2 = ILT::ColourObject->new_volume_colouring(
+                           ILT::IntersectionObject->new( $plane_object2,
+                                                         $volume_object ),
+                           $volume_object, Hot_metal_scale, 50, 150 );
+    $coloured_slice2->under_colour( "transparent" );
+
+    $coloured_slice3 = ILT::ColourObject->new_volume_colouring(
+                           ILT::IntersectionObject->new( $plane_object3,
+                                                         $volume_object ),
+                           $volume_object, Hot_metal_scale, 50, 150 );
+    $coloured_slice3->under_colour( "transparent" );
+
+    $surface_object = ILT::GeometricObject->new( "Data/surf1.obj" );
+
+    $union = ILT::UnionObject->new( $coloured_slice1,
+                                    $coloured_slice2,
+                                    $coloured_slice3,
+                                    $surface_object );
+
+    $scene_object = ILT::RenderObject->new( $union );
+    $scene_object->lighting_state( True );
+
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(2,2), $image_info );
+
+#--------------------------------------------------------------------------
+#
+#   image[2][3] is a flat shaded view of the ellipsoid and a transformed
+#   (rotated by 45 degrees and shifted slightly) smooth shaded copy of it
+#
+#--------------------------------------------------------------------------
+
+    $view = ILT::View->new_canonical( Top_view );
+    $surface_object = ILT::GeometricObject->new( "Data/surf1.obj" );
+    $transformed_surface = ILT::TransformObject->new( $surface_object, 
+                                                      "Data/rotate.xfm" );
+
+    $render1 = ILT::RenderObject->new( $surface_object );
+    $render1->lighting_state( True );
+    $render1->surface_shading( Flat_shading );
+
+    $render2 = ILT::RenderObject->new( $transformed_surface );
+    $render2->lighting_state( True );
+    $render2->surface_shading( Smooth_shading );
+
+    $scene_object = ILT::UnionObject->new( $render1,$render2 );
+
+    $image_info = ILT::ImageInfo->new( $scene_object, $view );
+    $layout->image_info( $layout->row_col_to_index(2,3), $image_info );
 
 #--------------------------------------------------------------------------
 #
@@ -249,4 +352,4 @@
 #
 #--------------------------------------------------------------------------
 
-    $layout->generate_image( "smorgasbord.rgb", 900, 600 );
+    $layout->generate_image( "smorgasbord.rgb", 1100, 0 );
